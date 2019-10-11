4cd74be26336c7dc7d0d14675548e7d8cd066f28
Yudi Andela <yudhi.andhela@gmail.com>
2019-10-11 09:17:21 +0700

    Setup View Engine dengan ejs

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require('express')
+const route = require('./routes/router.js')
 
 const app = express()
+
+// Setup Views
+app.set('view engine', 'ejs')
+
+// Setup Route
+app.use(route)
 
 module.exports = app
